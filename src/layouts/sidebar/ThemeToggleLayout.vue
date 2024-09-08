a9823b6cc881d27404c0118a69e11b8498546a7b
Ahmad Yogi Al Fandi <alfandi0857@gmail.com>
2024-09-08 13:33:34 +0700

    feat: new toggle darkmode

---
+++ src/layouts/sidebar/ThemeToggleLayout.vue
@@ -0,0 +1,12 @@
+<script setup>
+
+const app = document.getElementById("app");
+const setDark = () => {
+  app.classList.toggle("dark")
+};
+</script>
+<template>
+  <div>
+    <el-button @click="setDark">Default</el-button>
+  </div>
+</template>
