d0aa3b29c472605037913170a4effcad4899da64
Ahmad Yogi Al Fandi <alfandi0857@gmail.com>
2024-09-19 08:09:54 +0700

    feat: home transition

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,22 +2,44 @@
 import ContentComponent from "@/components/home/ContentComponent.vue";
 import TheWelcomeComponent from "@/components/home/TheWelcomeComponent.vue";
 import MedsosComponent from "@/components/medsos/MedsosComponent.vue";
-import { ref } from "vue";
+import { onMounted, ref } from "vue";
+const isVisible = ref(false);
+
+onMounted(() => {
+  isVisible.value = true;
+  console.log(isVisible.value);
+});
 </script>
 
 <template>
-  <main class="mt-12 flex flex-col">
-    <div
-      class="flex items-end justify-center w-screen h-96 bg-[url('@/assets/icon/IconBg1.svg')] bg-no-repeat bg-top"
-    >
-      <img src="@/assets/icon/human1.svg" class="size-10/12" />
-    </div>
-    <div class="mx-6">
-      <TheWelcomeComponent class="text-black dark:text-color-4" />
-      <ContentComponent class="text-slate-600 dark:text-slate-400 mt-2" />
-    </div>
-    <div class="mx-6 my-3">
-      <MedsosComponent />
-    </div>
-  </main>
+  <Transition>
+    <main v-if="isVisible" class="mt-12 flex flex-col">
+      <div
+        class="flex items-end justify-center w-screen h-96 bg-[url('@/assets/icon/IconBg1.svg')] bg-no-repeat bg-top"
+      >
+        <img src="@/assets/icon/human1.svg" class="size-10/12" />
+      </div>
+      <div class="mx-6">
+        <TheWelcomeComponent class="text-black dark:text-color-4" />
+        <ContentComponent class="text-slate-600 dark:text-slate-400 mt-2" />
+      </div>
+      <div class="mx-6 my-3">
+        <MedsosComponent />
+      </div>
+    </main>
+  </Transition>
 </template>
+
+<style scoped>
+/* we will explain what these classes do next! */
+.v-enter-active,
+.v-leave-active {
+  transition: all 700ms ease;
+}
+
+.v-enter-from,
+.v-leave-to {
+  opacity: 0;
+  transform: translateX(-100px);
+}
+</style>
